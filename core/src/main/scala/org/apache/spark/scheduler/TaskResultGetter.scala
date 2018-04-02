284ba424272220b2179715cdd1c6da275615de83
wuyi <ngone_5451@163.com>
2018-04-02 23:05:12 +0800

    read TaskResultGetter

--- core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
@@ -30,6 +30,7 @@ import org.apache.spark.serializer.SerializerInstance
 import org.apache.spark.util.{LongAccumulator, ThreadUtils, Utils}
 
 /**
+ * 通过运行一个线程池来反序列化或者远程获取task的结果。
  * Runs a thread pool that deserializes and remotely fetches (if necessary) task results.
  */
 private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedulerImpl)
@@ -37,6 +38,7 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
 
   private val THREADS = sparkEnv.conf.getInt("spark.resultGetter.threads", 4)
 
+  // 创建一个拥有固定个数（默认4个）守护线程的线程池
   // Exposed for testing.
   protected val getTaskResultExecutor: ExecutorService =
     ThreadUtils.newDaemonFixedThreadPool(THREADS, "task-result-getter")
@@ -54,6 +56,7 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
     }
   }
 
+  // 排队(???)处理成功的tasks(可以多线程并发)
   def enqueueSuccessfulTask(
       taskSetManager: TaskSetManager,
       tid: Long,
@@ -63,9 +66,13 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
         try {
           val (result, size) = serializer.get().deserialize[TaskResult[_]](serializedData) match {
             case directResult: DirectTaskResult[_] =>
+              // 如果因为maxResultSize的限制，而无法获取更多的task的结果，则返回
               if (!taskSetManager.canFetchMoreResults(serializedData.limit())) {
                 return
               }
+              // 反序列化task的结果（directResult），但是这里并没有获取该反序列化的结果，只是在
+              // 内部完成了反序列化的过程。等待之后TaskSetManager.handleSuccessfulTask()再去
+              // 获取该结果。
               // deserialize "value" without holding any lock so that it won't block other threads.
               // We should call it here, so that when it's called again in
               // "TaskSetManager.handleSuccessfulTask", it does not need to deserialize the value.
@@ -73,17 +80,30 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
               (directResult, serializedData.limit())
             case IndirectTaskResult(blockId, size) =>
               if (!taskSetManager.canFetchMoreResults(size)) {
+                // 如果累计的size超过maxResultSize，则删除该block
+                // TODO 难到我们不应该删除该TaskSet中所有已经完成的tasks
+                // 在之前通过IndirectTaskResult存储的的blocks吗？
+                // 答：不用了。看115L，在task的结果被driver成功获取之后，就会删除
+                // executor上的block
                 // dropped by executor if size is larger than maxResultSize
                 sparkEnv.blockManager.master.removeBlock(blockId)
                 return
               }
               logDebug("Fetching indirect task result for TID %s".format(tid))
+              // 标记该task正在获取结果，并通知DAG该事件，继而通知各个listeners该事件
               scheduler.handleTaskGettingResult(taskSetManager, tid)
+              // 通过BlockManager获取远程块（该远程块即为该task的结果）
               val serializedTaskResult = sparkEnv.blockManager.getRemoteBytes(blockId)
               if (!serializedTaskResult.isDefined) {
-                /* We won't be able to get the task result if the machine that ran the task failed
+                /* 获取task结果失败，有两种可能：
+                 * 一、刚刚运行该task的机器在task结束之后和我们尝试获取结果的期间挂掉了，
+                 * 所以我们无法获取该task的结果。
+                 * 二、block manager不得不flush该结果(??? 没看明白)
+                 * We won't be able to get the task result if the machine that ran the task failed
                  * between when the task ended and when we tried to fetch the result, or if the
                  * block manager had to flush the result. */
+                // 将该task以失败处理（失败的理由是：TaskResultLost（结果失联））, 会re-run
+                // TaskState是FINISHED???有问题么???
                 scheduler.handleFailedTask(
                   taskSetManager, tid, TaskState.FINISHED, TaskResultLost)
                 return
@@ -92,10 +112,15 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
                 serializedTaskResult.get.toByteBuffer)
               // force deserialization of referenced value
               deserializedResult.value(taskResultSerializer.get())
+              // 如果driver成功获取到task的结果，则删除在executor上存储的task的结果
               sparkEnv.blockManager.master.removeBlock(blockId)
               (deserializedResult, size)
           }
 
+          // 在从executors那边接收过来的InternalAccumulator.RESULT_SIZE这个accumulator中设置
+          // task的result size。我们之所以在driver这里设置result的size，是因为：如果我们在executors
+          // 上设置，那么在size更新后，需要重新序列化task的result。
+          // size在什么时候会更新??? 从executor的task结果返回处理的过程看，size没有更新啊，什么意思不懂。
           // Set the task result size in the accumulator updates received from the executors.
           // We need to do this here on the driver because if we did this on the executors then
           // we would have to serialize the result again after updating the size.
@@ -109,7 +134,11 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
               a
             }
           }
-
+          // 通知scheduler处理成功的task，scheduler继而会交给TaskSetManager来处理
+          // 注意这个过程是同步的（handleSuccessfulTask()会加锁）。想想，为什么这里会同步???
+          // 其实，TaskSetManager这个类的设计就要求TaskScheduler加锁来调用它的方法，以保证
+          // 线程安全。TaskSetManager维护了多个和task相关的数据结构，所以要加锁同步啊，以免
+          // 破坏这些数据结构。
           scheduler.handleSuccessfulTask(taskSetManager, tid, result)
         } catch {
           case cnf: ClassNotFoundException =>
